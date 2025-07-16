b1b378a145f9ff05682bbb952ac5aec45564ca59
salma-jnt <salmajanatiidrissi7@gmail.com>
2025-07-16 15:38:11 +0100

    Phase 0 : Init back + front

---
+++ back/missions/missions/src/main/java/com/ocp/missions/MissionsApplication.java
@@ -0,0 +1,13 @@
+package com.ocp.missions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MissionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MissionsApplication.class, args);
+	}
+
+}
---
+++ back/missions/missions/src/test/java/com/ocp/missions/MissionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ocp.missions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MissionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
